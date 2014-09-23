2e8bab8103da22708a1bf386afc8fef338c5dad8
Jaws <joris.broll@gmail.com>
2014-09-23 10:06:05 +0200

    Heroku test 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
 
+def sign_in(user)
+    cookies[:user_id] = user.id
+    self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+  def current_user?(user)
+    current_user == user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: cookies[:user_id])
+  end
 
   #include SessionsHelper
 
