057aee57e07d691e9ed4a33d5ab96fd045c6210d
Jaws <joris.broll@gmail.com>
2014-10-24 15:47:01 +0200

    24-10-2014 before heroku 2

--- db/migrate/20140929084819_clean_tables.rb
+++ db/migrate/20140929084819_clean_tables.rb
@@ -3,6 +3,5 @@ class CleanTables < ActiveRecord::Migration
   	rename_column :promocodes, :effect, :effect_type
   	add_column :promocodes, :effect_type_value, :integer
   	rename_column :promocodes, :effect_strengh, :effect_length_value
-  	drop_table :sub_companies
   end
 end
