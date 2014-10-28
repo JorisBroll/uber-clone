cafd6b4ba5e358e0a16abd42d86b21a5eba693b2
Jaws <joris.broll@gmail.com>
2014-10-28 16:36:08 +0100

    Release 2.3.8

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -12,15 +12,15 @@ class Admin::CoursesController < ApplicationController
 	end
 	def create
 		@course = Course.new(course_params)
-		#if @course.save
-		#	flash[:success] = "La course n°"+@course.id.to_s+" a été créée."
-			#AppLogger.log ({'user_id' => @current_user, 'action' => 'created', 'target_object' => {'type' => 'course', 'id' => @course.id.to_s} })
+		if @course.save
+			flash[:success] = "La course n°"+@course.id.to_s+" a été créée."
+			AppLogger.log ({'user_id' => @current_user, 'action' => 'created', 'target_object' => {'type' => 'course', 'id' => @course.id.to_s} })
 			redirect_to admin_courses_path
-	    #else
-	    #	flash[:error] = "La course n'a pas pu être créée, veuillez réessayer :"
-	    #	AppLogger.log ({'user_id' => @current_user, 'action' => 'fail_created', 'target_object' => {'type' => 'course'} })
-	    #	render 'new'
-	    #end
+	    else
+	    	flash[:error] = "La course n'a pas pu être créée, veuillez réessayer :"
+	    	AppLogger.log ({'user_id' => @current_user, 'action' => 'fail_created', 'target_object' => {'type' => 'course'} })
+	    	render 'new'
+	    end
 
 	end
 	def edit
@@ -51,7 +51,7 @@ class Admin::CoursesController < ApplicationController
 		private
 
 		    def course_params
-		    	#params['course']['stops'] = params['course']['stops'].to_json
+		    	params['course']['stops'] = params['course']['stops'].to_json
 		    	params.require(:course).permit(:from, :to, :date_when, :time_when, :computed_distance, :computed_duration, :computed_price, :stops, :nb_people, :user_id, :partner_id, :car_id, :company_id, :status, :notes, :payment_when, :created_by, :company_id, :payment_type, :flight_number)
 		    end
 
