1040487506ecd03ab930fa1f1afa5b9680444c72
Jaws <joris.broll@gmail.com>
2014-11-19 17:56:00 +0100

    Release 2.20

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -24,7 +24,22 @@ module CoursesHelper
 	def price_afterPromo(course, to = false)
 		price = course.computed_price
 
-		
+		if !course.promocode.nil?
+			case course.promocode.effect_type
+			when 'percent'
+				price = course.computed_price - course.computed_price*(course.promocode.effect_type_value.to_f/100)
+			when 'fixed'
+				price = course.computed_price - course.promocode.effect_type_value
+			end
+		end
+
+		case to
+			when 'naveco'
+				shareToPartner = (course.computed_price*((100-course.commission.to_f)/100))
+				price = course.computed_price - shareToPartner
+			when 'partner'
+				price = (course.computed_price*((100-course.commission.to_f)/100))
+		end
 		return price
 	end
 end
