0556dd73945b45a7bf2e4737565162a4baae91eb
Jaws <joris.broll@gmail.com>
2015-02-14 10:51:54 +0100

    Release 2.8.2

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "h4c4ny7qv7rsfcwy"
-Braintree::Configuration.public_key = "zqnk6yd49ns8j452"
-Braintree::Configuration.private_key = "bbc20d8ec239fdd90fa72e824b0dffcc"
+Braintree::Configuration.merchant_id = ENV["BRAINTREE_MERCHANT_ID_SB"]
+Braintree::Configuration.public_key = ENV["BRAINTREE_PUBLIC_KEY_SB"]
+Braintree::Configuration.private_key = ENV["BRAINTREE_PRIVATE_KEY_SB"]
