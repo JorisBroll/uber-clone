f1a2eac505461a20a12d32386f6440c6d7a5ada1
Jaws <joris.broll@gmail.com>
2015-02-20 17:35:52 +0100

    Release 2.8.38

--- app/controllers/app_calls_controller.rb
+++ app/controllers/app_calls_controller.rb
@@ -225,7 +225,7 @@ class AppCallsController < ApplicationController
 			rData = {}
 
 			if @account_type == "driver"
-				rData[:courses] = @user.drives_courses.where('status = ? OR status = ?', Course.statuses[:inactive], Course.statuses[:in_progress]).order(date_when: :desc, time_when: :desc).select(:id, :from, :to, :status, :course_type, :date_when, :time_when, :trip_wait_start, :trip_started, :trip_finished, :behavior_feedback, :user_id, :stops, :created_at)
+				rData[:courses] = @user.drives_courses.where('status = ? OR status = ?', Course.statuses[:inactive], Course.statuses[:in_progress]).order(date_when: :desc, time_when: :desc).select(:id, :from, :to, :status, :course_type, :date_when, :time_when, :trip_wait_start, :trip_started, :trip_finished, :behavior_feedback, :user_id, :stops, :final_price, :created_at)
 				rendering(rData, {:user => {:only => [:id, :name, :last_name, :cellphone, :email, :photo_url], :methods => [:photo_url] }}, nil, [:date_when_s, :time_when_s, :trip_wait_start_sec, :trip_started_sec, :trip_finished_sec])
 			elsif @account_type == "client"
 				rData[:courses] = {
@@ -924,6 +924,7 @@ class AppCallsController < ApplicationController
 			if course
 				course.trip_finished = Time.now
 				course.stops_price = params['stops_price']
+				course.final_price = course.computed_price + course.stops_price
 				if course.save
 					Log.create(user_id: @user.id, target_type: 1, target_id: course.id, action: 'end');
 					rData[:status] = true
@@ -949,7 +950,6 @@ class AppCallsController < ApplicationController
 
 			if course
 				course.behavior_feedback = params['feedback_value']
-				course.final_price = course.computed_price + course.stops_price
 				if course.save
 					Log.create(user_id: @user.id, target_type: 1, target_id: course.id, action: 'feedback');
 					rData = {
